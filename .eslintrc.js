f398efca355f82a6b7c65f06a247f0b7427983fe
lifuyuan-cx <lifuyuan-cx@huafang.com>
2022-09-20 12:48:31 +0800

    feat: 新增规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    // 一个环境定义了一组预定义的全局变量
+    node: true,
+    browser: true,
+    commonjs: true,
+    es6: true // 启用除module外所有es6特性
   },
   extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
+  parserOptions: { // ESLint 允许你指定你想要支持的 JavaScript 语言选项
+    parser: '@babel/eslint-parser', // 解析器
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
