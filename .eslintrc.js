8e4e09ad2c0140bd4baee6a362e2afee79c7ef13
lifuyuan-cx <lifuyuan-cx@huafang.com>
2022-09-20 18:37:51 +0800

    style(custom): 添加代码格式化相关配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    // 一个环境定义了一组预定义的全局变量
-    node: true,
-    browser: true,
-    commonjs: true,
-    es6: true // 启用除module外所有es6特性
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: { // ESLint 允许你指定你想要支持的 JavaScript 语言选项
-    parser: '@babel/eslint-parser', // 解析器
-    requireConfigFile: false
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
-      }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false
+    },
+
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4]
     }
-  ]
 }
